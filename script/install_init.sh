78f15678353c045513fcbe3ffae3885378db5f1e
韩文昊 <85087688@qq.com>
2024-12-08 19:12:39 +0800

    fzf

--- script/install_init.sh
+++ script/install_init.sh
@@ -9,3 +9,4 @@ yum install -y ncurses ncurses-devel libtool libtool-ltdl libtool-ltdl-devel
 yum install -y libxml2 libxml2-devel libxslt libxslt-devel
 yum install -y gd gd-devel libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel
 yum install -y neofetch htop iftop iotop net-tools wget curl git unzip zip
+yum install -y fzf jq tree
